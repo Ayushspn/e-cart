10a7015161531cdeef650b6ed29eac38997b5437
ayushmishraims@gmail.com <ayushmishraims@gmail.com>
2020-01-28 19:14:23 +0530

    commit with changes

--- src/Components/Shopping-List/Shopping-List.component.js
+++ src/Components/Shopping-List/Shopping-List.component.js
@@ -1,32 +1,32 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import ShoppingListItem from '../Shopping-Item/Shopping-item.component';
 import './shopping-List.styles.scss';
-import {SetSpinnerFlag} from '../../redux/ShopList/ShopActionCreators'
+import { SetSpinnerFlag } from '../../redux/ShopList/ShopActionCreators'
 
-const ShoppingList = ({ ShopItemList, setSpinner}) => {
+const ShoppingList = ({ ShopItemList, setSpinner }) => {
     useEffect(() => {
         setSpinner(true)
-    },[])
+    }, [])
 
     useEffect(() => {
-        if(ShopItemList && ShopItemList.length > 0){
-           setSpinner(false)
+        if (ShopItemList && ShopItemList.length > 0) {
+            setSpinner(false)
         }
-    },[ShopItemList.length])
+    }, [ShopItemList.length])
     return (
         <div className='shopping-list-container'>
-        {ShopItemList  &&  ShopItemList.length > 0 ?  ShopItemList.map((items) => {
-            return (
-                <ShoppingListItem 
-                key={items.id} 
-                items = {items}
-                type = 'shoppingItem' 
-                cmpType = 'shopItem'
-                />
-            )
-        })
-        : <div className ='no-records'> No Record Found</div>}
+            {ShopItemList && ShopItemList.length > 0 ? ShopItemList.map((items) => {
+                return (
+                    <ShoppingListItem
+                        key={items.id}
+                        items={items}
+                        type='shoppingItem'
+                        cmpType='shopItem'
+                    />
+                )
+            })
+                : <div className='no-records'> No Record Found</div>}
         </div>
     )
 }
@@ -39,7 +39,7 @@ const mapStateToProps = ({ shop: { ShopItemList } }) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        setSpinner : (flag) => dispatch(SetSpinnerFlag(flag))
+        setSpinner: (flag) => dispatch(SetSpinnerFlag(flag))
     }
 }
 
